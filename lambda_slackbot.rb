12c4866f9a51f3866f5aa937480f6f0dfc73d6d1
yuina_1056 <yuina.i.1056@gmail.com>
2019-01-18 00:49:04 +0900

    一旦戻す

--- lambda_slackbot.rb
+++ lambda_slackbot.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'slack'
+require 'dotenv'
 
 def slack_post(channel, message)
+  Dotenv.load
   Slack.configure do |config|
     config.token = ENV['SLACK_TOKEN']
   end
@@ -10,7 +14,7 @@ end
 
 def lambda_handler(event:, context:)
   slack_post('#general', 'testMessage')
-  # TODO implement
+  # TODO: implement
   { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
 end
-slack_post('#general', 'testMessage')
+p slack_post('#general', 'testMessage')
