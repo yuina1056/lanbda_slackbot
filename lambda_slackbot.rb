1485a21d651789884a6ca026256ff50ae0df115e
yuina1056 <yuina.i.1056@gmail.com>
2019-01-29 22:38:09 +0900

    test

--- lambda_slackbot.rb
+++ lambda_slackbot.rb
@@ -1,8 +1,10 @@
 require 'slack-ruby-client'
+require 'dotenv'
 require 'json'
 
 def slack_post
   Slack.configure do |conf|
+    Dotenv.load
     conf.token = ENV['SLACK_TOKEN']
   end
 
@@ -11,4 +13,9 @@ def slack_post
   client.chat_postMessage(channel: '#general', text: 'Hello World')
 end
 
+def lambda_handler(event:, context:)
+  slack_post
+  # TODO: implement
+  { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
+end
 slack_post
